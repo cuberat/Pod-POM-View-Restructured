65a6ce3c4de34ebfcf649478cca74e7ea263cf84
Alex Muntada <alexm@debian.org>
2018-04-07 23:46:11 +0200

    Update authors and copyright

--- lib/Pod/POM/View/Restructured.pm
+++ lib/Pod/POM/View/Restructured.pm
@@ -1,7 +1,9 @@
 # AUTHOR: Don Owens <don@regexguy.com>
 # AUTHOR: Jeff Fearn <Jeff.Fearn@gmail.com>
+# AUTHOR: Alex Muntada <alexm@cpan.org>
 # OWNER: 2010 Don Owens <don@regexguy.com>
 # OWNER: 2016 Jeff Fearn <Jeff.Fearn@gmail.com>
+# OWNER: 2016-2018 Alex Muntada <alexm@cpan.org>
 # LICENSE: Perl_5
 
 =pod
